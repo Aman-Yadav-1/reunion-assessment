3107ac4908dbc9749b42cbfff4ff8a47735bd762
Aman-Yadav-1 <amxn.yadav@gmail.com>
2025-01-23 05:11:06 +0530

    try

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // Enable CSS optimization
-  optimizeCss: true,
-  // Ensure CSS modules work
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    })
-    return config
-  },
+  swcMinify: true
 }
 
-export default nextConfig
+export default nextConfigexport default nextConfig
